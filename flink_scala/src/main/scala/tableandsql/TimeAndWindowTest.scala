e214654dde46574de55bc5026f3a35feb72bd0cf
xiaojiaenen <1163218095@qq.com>
2021-11-11 08:57:43 +0800

    flink

--- flink_scala/src/main/scala/tableandsql/TimeAndWindowTest.scala
+++ flink_scala/src/main/scala/tableandsql/TimeAndWindowTest.scala
@@ -1,5 +1,38 @@
 package tableandsql
 
+import apitest.SensorReading
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
+
 object TimeAndWindowTest {
+  def main(args: Array[String]): Unit = {
+
+    val env=StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+
+
+    val tableEnv=StreamTableEnvironment.create(env)
+
+
+    val stream = env.readTextFile("/home/xiaojia/Projects/IDEAProject/FlinkStudy/flink_scala/src/main/resources/data.txt")
+    val dataStream = stream.map(
+      data => {
+        val arr = data.split(",")
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+      }
+    )
+
+
+    val sensorTable = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timeStamp, 'pt.proctime)
+    sensorTable.printSchema()
+    sensorTable.toAppendStream[Row].print()
+
+    env.execute()
+
+
+  }
 
 }
